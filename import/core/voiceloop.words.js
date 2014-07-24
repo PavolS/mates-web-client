130bdfe2c829765b8eb3b57316563f6b05ac8195
Pavol Safarik <pavol.safarik@gmail.com>
2014-07-25 01:16:12 +0200

    with copy instead of link for import

---
+++ import/core/voiceloop.words.js
@@ -0,0 +1,20 @@
+var wordList = [
+	["SOM", "S AA M"], 
+	["ON", "AA N"], ["ON(1)", "AO N"], 
+	["LOOP", "L UW P"], 
+	["ONE", "W AH N"], ["ONE(1)", "HH W AH N"], 
+	["REPEAT", "R IH P IY T"], ["REPEAT(1)", "R IY P IY T"], 
+	["THAT", "DH AE T"], ["THAT(1)", "DH AH T"], 
+	["GOOD", "G UH D"], ["GOOD(1)", "G IH D"], 
+	["MORNING", "M AO R N IH NG"], 
+	["HELLO", "HH AH L OW"], ["HELLO(1)", "HH EH L OW"], 
+	["HI", "HH AY"], 
+	["INVESTIGATE", "IH N V EH S T AH G EY T"], 
+	["PARAMETER", "P ER AE M AH T ER"], 
+	["SPELLING", "S P EH L IH NG"], 
+	["CHANGE", "CH EY N JH"], 
+	["LIMITS", "L IH M AH T S"], ["LIMITS(1)", "L IH M IH T S"], 
+	["EVERYONE", "EH V R IY W AH N"], 
+	["SPACON", "S P EY K AA N"], 
+	["SOE", "S AW IY"] 
+];
